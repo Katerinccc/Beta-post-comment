8c0f224bc9c17f0a7b940554af4be1d1bf31d3ee
Katerin Calderon <kccc0713@gmail.com>
2022-09-13 15:09:26 -0500

    add dockerfile and Environment Variables to deploy project in heroku

--- src/main/java/com/posada/santiago/betapostsandcomments/imp/ApprenticesBetaPostsCommentsApplication.java
+++ src/main/java/com/posada/santiago/betapostsandcomments/imp/ApprenticesBetaPostsCommentsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApprenticesBetaPostsCommentsApplication {
+public class  ApprenticesBetaPostsCommentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApprenticesBetaPostsCommentsApplication.class, args);
--- src/main/java/com/posada/santiago/betapostsandcomments/imp/application/adapters/repository/MongoViewRepository.java
+++ src/main/java/com/posada/santiago/betapostsandcomments/imp/application/adapters/repository/MongoViewRepository.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-
 @Repository
 public class MongoViewRepository implements DomainViewRepository {
 
